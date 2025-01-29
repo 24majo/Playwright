940630f043a260706ddbd4c1ad60d9e8453a9b8c
Alondra <alondra.guerrero@emeraldigital.com>
2025-01-29 16:37:44 -0600

    torneos

--- tests/torneos.spec.ts
+++ tests/torneos.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach("Inicios de sesion", async ({ page }) => {
     await page.goto('http://localhost:3000/auth');
-    await page.getByTestId('inputCorreo').fill('majo76@prueba.com');
+    await page.getByTestId('inputCorreo').fill('majo84@prueba.com');
     await page.getByTestId('inputPassword').fill('12345678');
     await page.getByTestId('crearCuenta').click();
     await page.getByRole('link', { name: 'Mis torneos' }).click()
@@ -43,33 +43,23 @@ test("Crear", async ({ page }) => {
     await page.pause();
 })
 
-test("Eliminar", async ({ page }) => {
-    await page.pause()
+test("Editar", async ({ page }) => {
+    await page.getByRole('button', {name: 'Crear torneo'}).click({ force: true})
+    await page.getByRole('row', { name: /^formal/ }).getByRole('button').nth(1).click({ force: true })
+    //await page.getByTestId('inputNombreCompetencia').nth(2).fill('')
+    
     var formato = ['Eliminación directa','Eliminación directa (ida y vuelta)','Liga','Liga (ida y vuelta)'];
-    for (let i = 0; i < formato.length; i++) {
-        var op_formato = await expect(page.getByText(formato[i], { exact: true }).first()).toBeVisible()
-        await page.getByRole('row', { name: '/' + op_formato + '.*/' }).getByRole('button').first().click();
-        //console.log(op_formato)
-        //const opcionLocator = page.locator(`text=${formato[i]}`);
-        // if (await opcionLocator.isVisible()) {
-        //     console.log("Visible")
-        //     await page.getByRole('row', { name: '/'+opcionLocator+'.*/' }).getByRole('button').first().click();
-        //     await page.pause()
-        //     break; // Sale del bucle una vez que ha hecho click en el primer elemento visible
-        // }
-        // else{
-        //     await page.getByRole('button', { name: 'Next Page' }).click();
-        //     await page.pause()
-        // }
-    }
-    /*
-        await page.getByRole('row', { name: 'formal Liga (ida y vuelta)' }).getByRole('button').nth(2).click();
-        await page.getByRole('button', { name: 'Previous Page' }).click();
-        await page.getByRole('button', { name: 'Next Page' }).click();
-        await page.getByRole('button', { name: 'First Page' }).click();
-        await page.getByRole('button', { name: 'Last Page' }).click();
-        await page.getByText('Liga (ida y vuelta)').click();
-        await page.getByRole('row', { name: 'formal Liga (ida y vuelta)' }).getByRole('button').nth(1).click();
+    var t_formato = formato[Math.floor(Math.random() * formato.length)];
+    console.log("Formato: " + t_formato)
+    await page.getByTestId('selectFormatoCompetencia').nth(2).click({ force: true })
+    await page.waitForTimeout(1000)
+    await page.getByRole('option', { name: t_formato, exact: true }).click({ force: true })
+    //await page.getByRole('button', { name: 'Guardar cambios' }).nth(2).click({ force: true })
+    await page.pause()
+})
 
-    */
+test("Eliminar", async ({ page }) => {
+    await page.getByRole('button', {name: 'Crear torneo'}).click({ force: true})
+    await page.getByRole('row', { name: /^formal/ }).getByRole('button').nth(2).click({ force: true })
+    await page.pause()
 })
