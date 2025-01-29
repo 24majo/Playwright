762fba54ae98e76d857ac8201290e4bb79849b13
Alondra <alondra.guerrero@emeraldigital.com>
2025-01-29 12:01:42 -0600

    equipos

--- tests/equipos.spec.ts
+++ tests/equipos.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe("CaskrApp", async() => {
     test.beforeEach("Inicios de sesión", async ({ page }) => {
         await page.goto('http://localhost:3000/auth');
-        await page.getByTestId('inputCorreo').fill('majo67@prueba.com'); 
+        await page.getByTestId('inputCorreo').fill('majo84@prueba.com'); 
         await page.getByTestId('inputPassword').fill('12345678');
         await page.getByTestId('crearCuenta').click();
         await page.waitForTimeout(1000); 
@@ -11,47 +11,41 @@ test.describe("CaskrApp", async() => {
 
     test("Agregar", async ({ page }) => {
         // Caso 1: Agregar un equipo con éxito
-        // Caso 2: NO es posible agregar por partidos agendados
+        // Caso 2: No es posible agregar por partidos agendados
+        // Caso 3: Límite de equipos alcanzado 
 
         await page.getByRole('link', { name: 'Equipos' }).click()
 
-        var nota = await page.locator("No puedes añadir un equipo porque tienes partidos agendados")
-        var boton = await page.locator('//span[text()="Agregar equipo"]').isEnabled()
-        console.log(boton)
+        for(var i = 0; i < 3; i++){
+            await page.click('//span[text()="Agregar equipo"]')
+            await page.waitForTimeout(2000)
+            var n_equipo = Math.floor(Math.random() * 20)
+            await page.locator('//input[@name="nombre"]').fill("Equipo " + n_equipo);
 
-        if(boton) {
-            console.log("Caso 2")
-        }
-        else{
-            for(var i = 0; i < 4; i++){
-                await page.click('//span[text()="Agregar equipo"]')
-                var n_equipo = Math.floor(Math.random() * 20)
-                await page.locator('//input[@name="nombre"]').fill("Equipo " + n_equipo);
+            var nombres = ['Lucero', 'Sofía', 'Frida', 'Matías', 'Eliseo', 'Cinthia', 'Édgar', 'Enrique', 'Julio', 'César', 'Leonardo', 'Ramses', 'Juan', 'David'];
+            var nombre = nombres[Math.floor(Math.random() * nombres.length)];
+            await page.locator('//input[@name="nombre_capitan"]').fill(nombre);
 
-                var nombres = ['Lucero', 'Sofía', 'Frida', 'Matías', 'Eliseo', 'Cinthia', 'Édgar', 'Enrique', 'Julio', 'César', 'Leonardo', 'Ramses', 'Juan', 'David'];
-                var nombre = nombres[Math.floor(Math.random() * nombres.length)];
-                await page.locator('//input[@name="nombre_capitan"]').fill(nombre);
+            var apellidos = ['Albor', 'Salazar', 'Rodríguez', 'Cuevas', 'Solórzano', 'Ramos', 'Delgado', 'Ávalos', 'Ruiz', 'Segoviano'];
+            var apellido = apellidos[Math.floor(Math.random() * apellidos.length)];
+            await page.locator('//input[@name="apellidos_capitan"]').fill(apellido);
 
-                var apellidos = ['Albor', 'Salazar', 'Rodríguez', 'Cuevas', 'Solórzano', 'Ramos', 'Delgado', 'Ávalos', 'Ruiz', 'Segoviano'];
-                var apellido = apellidos[Math.floor(Math.random() * apellidos.length)];
-                await page.locator('//input[@name="apellidos_capitan"]').fill(apellido);
+            var telefono = Math.floor(1000000000 + Math.random() * 9000000000).toString();
+            await page.locator('//input[@name="telefono"]').fill(telefono);
 
-                var telefono = Math.floor(1000000000 + Math.random() * 9000000000).toString();
-                await page.locator('//input[@name="telefono"]').fill(telefono);
+            await page.getByLabel('LigaAgregar nuevo equipo').getByRole('button', { name: 'Agregar equipo' }).click();
+            await page.waitForTimeout(5000)
 
-                await page.getByLabel('LigaAgregar nuevo equipo').getByRole('button', { name: 'Agregar equipo' }).click();
-                
-                var participantes = await page.getByLabel('Equipos participantes').innerText()
-                var p_lista = participantes.match(/Equipo \d+/g)
-                p_lista!.includes("Equipo " + n_equipo)
+            var participantes = await page.getByLabel('Equipos participantes').innerText()
+            var p_lista = participantes.match(/Equipo \d+/g)
+            p_lista!.includes("Equipo " + n_equipo)
 
-                var todos = await page.getByLabel('Todos los equipos').innerText()
-                var t_lista = todos.match(/Equipo \d+/g)
-                t_lista!.includes("Equipo " + n_equipo)
+            var todos = await page.getByLabel('Todos los equipos').innerText()
+            var t_lista = todos.match(/Equipo \d+/g)
+            t_lista!.includes("Equipo " + n_equipo)
 
-                await page.pause()
-                // await page.locator('//span[text()="Cancelar"]').click(); // Botón para cancelar acción
-            }
+            await page.pause()
+            // await page.locator('//span[text()="Cancelar"]').click(); // Botón para cancelar acción
         }
         // await page.pause();
     })
@@ -128,7 +122,5 @@ test.describe("CaskrApp", async() => {
         await page.waitForTimeout(2000)
         await page.getByRole('button', { name: 'Eliminar' }).nth(equipo!.length - 1).click();
         await page.pause()
-
-
     })
 })
